8f53ad0e809fa0acf8760bbae3e68b7d662ec296
Talita Duarte <taliarte01@gmail.com>
2021-11-22 10:08:02 -0300

    projeto hobby

---
+++ projeto-hobby/script.js
@@ -0,0 +1,16 @@
+let apresentacaoPessoal = document.getElementById('paragrafo-ap')
+apresentacaoPessoal.innerHTML =
+  'Oi, me chamo Talita Duarte, tenho 38 anos, sou baiana, natural de Itabun/BA, esposa, mãe e com espírito inquieto.'
+
+let titulo = document.getElementById('titulo')
+titulo.innerHTML = 'O meu hobby é costurar'
+
+let apresentacaoHobby = document.getElementById('paragrafo-justificativa')
+apresentacaoHobby.innerHTML =
+  'Desde pequena amava ficar olhando a minha mãe cosnturar, passava horas com ali observando cada detalhe. Era lindo como ela pegava naqueles tecidos. A forma como ela pegava na tesoura e arrumava o tecido sobre a mesa. Meus ocoração sempre se alegrou em ver todos aqueles movimentos. O cuidado com cada peça, o acabamento, a prega do botão, enfim, cada detalhe era registrado pelos meus olhos. E quando percebir, eu estava fazendo tudo aquilo e a minha primeira produção dfoi um lindo conjunto amarelo que fiz para minha mãe. Foi incrível e daí pra cá não parei mais. Quando quero me distrair, sento na máquina e vou produzir minhas peças de roupas.'
+
+let pergunta = document.getElementById('paragrafo-pergunta')
+pergunta.innerHTML = 'Sabe porque costurar é viver?'
+
+let resposta = document.getElementById('paragrafo-resposta')
+resposta.innerHTML = 'Sabe porque costurar é viver?'
