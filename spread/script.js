bfbe4dc491f40428357e96eea4ce140f18797ea7
Talita Duarte <85463337+Taliarte01@users.noreply.github.com>
2022-01-27 08:45:40 -0300

    utilizando spread

---
+++ spread/script.js
@@ -0,0 +1,23 @@
+numeros = [2, 1, 4, 2]
+
+function multiplica(num1, num2, num3, num4) {
+  return num1 * num2 * num3 * num4
+}
+
+console.log(multiplica(...numeros))
+
+function concatenaVetores(arr1, arr2) {
+  return [...arr1, ...arr2]
+}
+
+console.log(concatenaVetores([1, 3], [1, 4]))
+
+function maiorValor() {
+  const numeros = []
+  for (let i = 0; i < 10; i++) {
+    numeros.push(Math.floor(Math.random() * 100) + 1)
+  }
+  return `Maior numero em [${numeros}]: ${Math.max(...numeros)}`
+}
+
+console.log(maiorValor())
