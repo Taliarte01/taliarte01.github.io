a1f20ba9c9b122073011ed9d786fb148de34751d
Talita Duarte <85463337+Taliarte01@users.noreply.github.com>
2022-02-25 07:16:25 -0300

    melhorias

--- jogo-heman/script.js
+++ jogo-heman/script.js
@@ -42,7 +42,7 @@ class Hero {
 let lutador1
 let lutador2
 
-//objato com a discrição dos personagens
+//objeto com a discrição dos personagens
 const herois = {
   heman: {
     nome: 'heman',
@@ -89,7 +89,18 @@ const herois = {
   }
 }
 
+// recalcula os atributos dos herois ao dar o reset na partida
+function balanceiaAtributos() {
+  Object.keys(herois).forEach(heroi => {
+    herois[heroi].forca = Math.floor(Math.random() * 100) + 1
+    herois[heroi].inteligencia = Math.floor(Math.random() * 100) + 1
+    herois[heroi].agilidade = Math.floor(Math.random() * 100) + 1
+  })
+}
+
 //funções
+let caixaJogador1
+let jogadorSelecionado1
 function escolherLutador1(_nomeHeroi) {
   lutador1 = herois[_nomeHeroi]
   const jogadores1 = document.getElementsByClassName('jogador1')
@@ -97,7 +108,7 @@ function escolherLutador1(_nomeHeroi) {
     jogador1.style = 'borde: none;'
   }
   const jogador = document.getElementById(_nomeHeroi)
-  jogador.style = 'border: 5px solid blue;'
+  jogador.style = 'border: 5px solid blue;  background-color: rgba(0,0,255,0.3)'
 
   const jogadorSelecionado = document.getElementById('lutador-sele-1')
   jogadorSelecionado.setAttribute('src', jogador.getAttribute('src'))
@@ -106,6 +117,8 @@ function escolherLutador1(_nomeHeroi) {
   caixaJogador.style = 'visibility: visible;'
 }
 
+let caixaJogador2
+let jogadorSelecionado2
 function escolherLutador2(_nomeHeroi) {
   lutador2 = herois[_nomeHeroi]
   //contorno do personagem selecionado
@@ -114,14 +127,14 @@ function escolherLutador2(_nomeHeroi) {
     jogador2.style = 'borde: none;'
   }
   const jogador = document.getElementById(_nomeHeroi)
-  jogador.style = 'border: 5px solid red;'
+  jogador.style = 'border: 5px solid red; background-color: rgba(255,0,0,0.3);'
 
   //Mostra o usuário selecionado no ring
-  const jogadorSelecionado = document.getElementById('lutador-sele-2')
-  jogadorSelecionado.setAttribute('src', jogador.getAttribute('src'))
+  jogadorSelecionado2 = document.getElementById('lutador-sele-2')
+  jogadorSelecionado2.setAttribute('src', jogador.getAttribute('src'))
 
-  const caixaJogador = document.getElementById('selecao-lutador-2')
-  caixaJogador.style = 'visibility: visible;'
+  caixaJogador2 = document.getElementById('selecao-lutador-2')
+  caixaJogador2.style = 'visibility: visible;'
 }
 
 function atacar(jogador) {
@@ -130,18 +143,22 @@ function atacar(jogador) {
   if (jogador === 'jogador1') {
     attrLutador2.setAttack(attrLutador1.getAttack())
     const vida = document.getElementById('vida-jogador-2')
-    vida.innerHTML = attrLutador2.getVida()
+    // diminui a barra de vida
+    vida.style.setProperty('width', `${attrLutador2.getVida() * 2}px`)
 
     if (attrLutador2.getVida() <= 0) {
+      vencedor.style = 'visibility: visible;'
       vencedor.innerHTML = `${lutador1.nome.toUpperCase()} VENCEU`
       encerraJogo()
     }
   } else if (jogador === 'jogador2') {
     attrLutador1.setAttack(attrLutador2.getAttack())
     const vida = document.getElementById('vida-jogador-1')
-    vida.innerHTML = attrLutador1.getVida()
+    // diminui a barra de vida
+    vida.style.setProperty('width', `${attrLutador1.getVida() * 2}px`)
 
     if (attrLutador1.getVida() <= 0) {
+      vencedor.style = 'visibility: visible;'
       vencedor.innerHTML = `${lutador2.nome.toUpperCase()} VENCEU`
       encerraJogo()
     }
@@ -150,6 +167,9 @@ function atacar(jogador) {
 
 let attrLutador1
 let attrLutador2
+
+const botao = document.getElementById('botao')
+const comecar = document.getElementById('comecar')
 
 function lutar() {
   attrLutador1 = new Hero(
@@ -167,10 +187,23 @@ function lutar() {
     lutador2.inteligencia
   )
 
+  const vidas = document.getElementsByClassName('vida-jogador')
+  for (let vida of vidas) {
+    vida.style = 'visibility: visible;'
+  }
+
+  const barraVida1 = document.getElementById('barra-vida-1')
+  barraVida1.style = 'visibility: visible;'
+
+  const barraVida2 = document.getElementById('barra-vida-2')
+  barraVida2.style = 'visibility: visible;'
+
   const botoes = document.getElementsByClassName('botao-atacar')
   for (let botao of botoes) {
     botao.style = 'visibility: visible;'
   }
+  botao.style = 'display: none;'
+  comecar.style = 'display: inline;'
 }
 
 function encerraJogo() {
@@ -183,4 +216,49 @@ function encerraJogo() {
   for (let vida of vidas) {
     vida.style = 'visibility: hidden;'
   }
+
+  const barraVida1 = document.getElementById('barra-vida-1')
+  barraVida1.style = 'visibility: hidden;'
+
+  const barraVida2 = document.getElementById('barra-vida-2')
+  barraVida2.style = 'visibility: hidden;'
+}
+
+// reinicia a partida e todos os componentes do jogo
+function fightAgain() {
+  lutador1 = null
+  lutador2 = null
+  attrLutador1 = null
+  attrLutador2 = null
+
+  caixaJogador1 = document.getElementById('selecao-lutador-1')
+  caixaJogador1.style = 'visibility: hidden;'
+  jogadorSelecionado1 = document.getElementById('lutador-sele-1')
+  jogadorSelecionado1.removeAttribute('src')
+  const jogadores1 = document.getElementsByClassName('jogador1')
+  for (let jogador1 of jogadores1) {
+    jogador1.style = 'borde: none;'
+  }
+
+  caixaJogador2 = document.getElementById('selecao-lutador-2')
+  caixaJogador2.style = 'visibility: hidden;'
+  jogadorSelecionado2 = document.getElementById('lutador-sele-2')
+  jogadorSelecionado2.removeAttribute('src')
+  const jogadores2 = document.getElementsByClassName('jogador2')
+  for (let jogador2 of jogadores2) {
+    jogador2.style = 'borde: none;'
+  }
+
+  const botoes = document.getElementsByClassName('botao-atacar')
+  for (let botao of botoes) {
+    botao.style = 'visibility: hidden;'
+  }
+
+  const vencedor = document.getElementById('vencedor')
+  vencedor.style = 'visibility: hidden;'
+
+  botao.style = 'display: inline;'
+  comecar.style = 'display: none;'
+
+  balanceiaAtributos()
 }
